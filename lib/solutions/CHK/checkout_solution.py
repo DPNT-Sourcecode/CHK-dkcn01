756ee81e237c26b3a38d5dd6575056001a0bd18b
sbx_user1051 <sbx_user1051@169.254.103.133>
2024-07-05 13:42:12 +0000

    Fri Jul 05 13:42:12 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,14 +4,15 @@
 # skus = unicode string
 
 item_prices = {
-"A":{1:50, 3:130}
-"B":{1:30, 2:45}
-"C":{1:20}
-"D":{1:15}
+"A":{1:50, 3:130},
+"B":{1:30, 2:45},
+"C":{1:20},
+"D":{1:15},
 }
 
 def checkout(skus):
     item_count={}
+    total = 0
     for item in skus:
         current_item = item.upper()
         if current_item not in item_prices.keys():
@@ -26,4 +27,10 @@ def checkout(skus):
         i_prices = list(item_prices[item].keys())
         i_prices.reverse()
         for mcount in i_prices:
-            if item_count[item] >= 
+            if item_count[item] >= mcount:      # worth performing division
+                val = int(item_count[item]/mcount)
+                total += val * item_prices[item][mcount]
+                item_count[item] -= val
+                
+    return total
+
