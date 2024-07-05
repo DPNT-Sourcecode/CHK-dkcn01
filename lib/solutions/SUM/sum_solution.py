6bbb580e4ba8b8875f38152bfa08f7a4a378bc28
sbx_user1051 <sbx_user1051@169.254.105.229>
2024-07-05 11:04:48 +0000

    Fri Jul 05 11:04:48 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,17 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(x:int, y:int)-> int:
+    """
+    compute(x:int, y:int)-> int
+    
+    returns the sum of a and b
+    Note : 
+    a must be given an int with value between 0 and 100
+    b must be given an int with value between 0 and 100
+    """
+    assert type(a) is int, f"first argument must be an int, got: {type(a)}"
+    assert type(b) is int, f"second argument must be an int, got: {type(b)}"
+    assert 0<=a<=100, f"first argument must be between 0 and 100, got: {a}"
+    assert 0<=b<=100, f"second argument must be between 0 and 100, got: {b}"
+    
+    return int(a+b)
+
