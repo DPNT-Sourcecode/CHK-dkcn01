fe674534f9351c94d78150f107d0068e9b798617
sbx_user1051 <sbx_user1051@169.254.105.77>
2024-07-06 09:34:12 +0000

    Sat Jul 06 09:34:12 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -29,18 +29,43 @@ def calc_item_count(skus:str)-> dict:
                 item_count[item] += 1
     return item_count
     
-def calc_special_offers(item_count):
+def calc_special_offers_applicable(item_count:dict)->dict:
     special_offer_eligibility_count = {}
     for sp_offer_k in special_offers:
+        # sp_offer_k = E only
         sp_offer_divs = list(special_offers[sp_offer_k].keys())
         sp_offer_divs.reverse()
         for div in sp_offer_divs:
-            special_offers[sp_offer_k]
-            item_count[sp_offer_k]
-            
-    return sp_count
+            #print(div)
+            discount_multiple = int(item_count[sp_offer_k] / div)
+            if discount_multiple > 0:
+                #print(f"discount multiple : {discount_multiple}") # until here we are on track !
+                
+                
+                for item_letter in item_count:
+                    if item_letter in list(special_offers[sp_offer_k][div].keys()):
+                        #print("IN !")
+                        #print(special_offers[sp_offer_k][div].keys())
+                        while(discount_multiple>0):
+                            if item_letter in special_offer_eligibility_count:
+                                special_offer_eligibility_count[item_letter] += special_offers[sp_offer_k][div][item_letter]
+                                item_count[sp_offer_k] -=  div
+                                discount_multiple -= 1
+                            else:
+                                special_offer_eligibility_count[item_letter] = special_offers[sp_offer_k][div][item_letter]
+                                item_count[sp_offer_k] -=  div
+                                discount_multiple -= 1
+                    else:
+                        pass
+    return special_offer_eligibility_count
     
+def apply_spo_applicable(total:int, spo:dict)->int:
+    for letter in spo:
+        total += spo[letter]*item_prices[letter][1]
+    return total
+
 def calc_total(item_count):
+    total = 0
     for item in item_count:
         i_prices = list(item_prices[item].keys())
         i_prices.reverse()
@@ -57,11 +82,20 @@ def calc_total(item_count):
 def checkout(skus:str):
     assert type(skus) is str, "skus must be a string"
     item_count = calc_item_count(skus)
-    item_count_for_discounts = copy.deepcopy(item_count)
-    
-    print(item_count_for_discounts)
-    spo = calc_special_offers(item_count_for_discounts)
-    print(spo)
-    
-    total = calc_total(item_count)
-    return total
+    if item_count != -1:
+        item_count_for_discounts = copy.deepcopy(item_count)
+        
+        #print(f"item_count_for_discounts {item_count_for_discounts}")
+        spo_applicable = calc_special_offers_applicable(item_count_for_discounts)
+        #print(f"spo {spo_applicable}")
+        
+        total = calc_total(item_count)
+        #print(f"total {total}")
+        
+        total = apply_spo_applicable(total, spo_applicable)
+        #print(f"total {total}")
+        
+        return total
+    else:
+        #print(-1)
+        return -1
